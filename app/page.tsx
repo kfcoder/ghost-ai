cba6e30727e65c3bc2ffdd5c58066281e26c5db5
giuseppe marinelli <giuseppe.marinelli89@gmail.com>
2026-09-17 18:29:53 +0200

    #5 prova bottone

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">Ghost AI</div>
+    <div className="flex h-screen items-center justify-center">
+      <p>Ghost AI</p>
+      <Button>Click me</Button>
+    </div>
+   
   );
 }
