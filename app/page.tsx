057db4eb8eb43109b6ea3f4972c73c369a9c3d40
giuseppe marinelli <giuseppe.marinelli89@gmail.com>
2026-09-17 20:09:13 +0200

    #6 completamento design system e pulizia page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
       <p>Ghost AI</p>
-      <Button>Click me</Button>
+      
     </div>
    
   );
